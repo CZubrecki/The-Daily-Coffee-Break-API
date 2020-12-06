002b44e90f35930b7c9e286659786c92b7ef3cba
CZubrecki <c.m.zubrecki@gmail.com>
2020-12-06 13:51:15 -0600

    Update to split

--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -12,14 +12,16 @@ export class PhotoService {
         });
 
         const base64String = 'data:image/png;base64,' + data.base64;
-        const imageBuffer = Buffer.from(base64String, "base64")
+        const base64string = base64String.split(',')[1];
+        const imageBuffer = Buffer.from(base64string, 'base64');
+        console.log(imageBuffer);
 
         await worker.load();
         await worker.loadLanguage('eng');
         await worker.initialize('eng');
         console.log('WE ARE HERE');
         const { data: { text } } = await worker.recognize(imageBuffer);
-        console.log('--------', data);
+        console.log('--------', text);
         await worker.terminate();
         return text;
     }
