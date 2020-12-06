e5112817ec30816cba0592781cd7d2e55e3d7eda
CZubrecki <c.m.zubrecki@gmail.com>
2020-12-06 13:15:49 -0600

    Try to parse body

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const bodyParser: any = null;
+  app.use(bodyParser.json({ limit: '10mb', extended: true }))
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
   await app.listen(process.env.PORT);
 }
 bootstrap();
