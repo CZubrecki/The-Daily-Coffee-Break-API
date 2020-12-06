b4bcdd69ba58e085800be0bfbbeca195aa4a59b7
CZubrecki <c.m.zubrecki@gmail.com>
2020-12-06 13:55:10 -0600

    PLEASE WORK

--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -7,22 +7,14 @@ export class PhotoService {
     ) { }
 
     public async processImage(data: any): Promise<any> {
-        const worker = Tesseract.createWorker({
-            logger: m => console.log(m)
-        });
 
         const base64String = 'data:image/png;base64,' + data.base64;
         const base64string = base64String.split(',')[1];
         const imageBuffer = Buffer.from(base64string, 'base64');
         console.log(imageBuffer);
-
-        await worker.load();
-        await worker.loadLanguage('eng');
-        await worker.initialize('eng');
         console.log('WE ARE HERE');
-        const { data: { text } } = await worker.recognize(imageBuffer);
+        const { data: { text } } = await Tesseract.recognize(Buffer.from(base64string, 'base64'));
         console.log('--------', text);
-        await worker.terminate();
         return text;
     }
 
