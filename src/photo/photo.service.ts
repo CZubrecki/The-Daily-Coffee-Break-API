1936955a19fc10d2139ef367f798beeb8ec3f598
CZubrecki <c.m.zubrecki@gmail.com>
2020-12-06 13:30:52 -0600

    Update return and logs

--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -6,16 +6,18 @@ export class PhotoService {
     constructor(
     ) { }
 
-    public async processImage(data: any): Promise<void> {
+    public async processImage(data: any): Promise<any> {
         const worker = Tesseract.createWorker({});
         const imageBuffer = Buffer.from(data.base64, "base64");
 
         await worker.load();
         await worker.loadLanguage('eng');
         await worker.initialize('eng');
+        console.log('WE ARE HERE');
         const { data: { text } } = await worker.recognize(imageBuffer);
         console.log('--------', text);
         await worker.terminate();
+        return text;
     }
 
 }
